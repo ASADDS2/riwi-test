c3d385ae79b191c0e2cede2f7d3ce2aa31cdac43
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2025-03-31 19:28:53 -0500

    Lo que sea

--- clase.py
+++ clase.py
@@ -1,31 +1,5 @@
-# contador = 0
-# suma = 0
-# numero_maximo = float (input("Introduce el numero maximo a sumar: "))
-# while contador < numero_maximo:
-#     suma += contador
-#     contador += 5
-# print(f"La suma es: {suma}")
-
-# tabla = float(input("Introduce la tabla de multiplicar que quieres ver: "))
-# inicio = 1
-
-# while inicio <= 10:
-#     resultado = tabla * inicio 
-#     print(tabla, "*",inicio, "=",resultado)
-#     inicio += 1
-
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
-
-my_function("Emil", "Tobias", "Linus")
-
-numero_inicial = float(input("Introduce tu numero inicial: "))
-numero_final = float(input("Ingresa tu numero final: "))
-
-cantidad = 0 
-while numero_inicial < numero_final:
-    if numero_inicial % 2 == 0:
-        print(numero_inicial)
-        cantidad += 1
-    numero_inicial += 1
-print("Hay", cantidad,"numeros pares")
+print("Bienvenido a la calculadora de grasa corporal")
+peso = float(input("Por favor ingrese su peso: "))
+estatura = float (input ("Por favor ingrese su estatura en metros: "))
+indice_de_grasa_corporal = peso / estatura ** 2
+print (f"Tu indice de grasa corporal es {indice_de_grasa_corporal}")
